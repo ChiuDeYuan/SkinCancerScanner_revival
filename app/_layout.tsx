0d5a3df59d4fb68f9e157c5e4d7c5bd05160762a
ChiuDeYuan <chiudeyuan10111223@gmail.com>
2025-02-04 08:25:32 +0800

    before prebuild

--- app/_layout.tsx
+++
@@ -1,6 +0,0 @@
-import { Stack } from "expo-router";
-import { View } from "react-native";
-
-export default function RootLayout() {
-  return <View style={{flex:1, backgroundColor: 'white'}}><Stack /></View>;
-}
--- app/tabs/diagnosis.tsx
+++ app/tabs/diagnosis.tsx
@@ -1,9 +1,39 @@
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { Dimensions, TouchableHighlight } from 'react-native';
+
+const FlaskTest = () => {
+  fetch('http://116.241.64.125:5000/')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.log(error));
+}
 
 const DiagnosisScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>Diagnosis Screen</Text>
+      <View style={styles.container}>
+        <Text>Diagnosis Screen</Text>
+      </View>
+
+      <View style={styles.container}>
+        <View style={circleStyle.circle}>
+          <TouchableHighlight
+          style = {{
+            borderRadius: Math.round(Dimensions.get('window').width + Dimensions.get('window').height) / 2,
+            width: Dimensions.get('window').width * 0.45,
+            height: Dimensions.get('window').width * 0.45,
+            backgroundColor:'#ffffff',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+          underlayColor = '#ccc'
+          onPress = { () => FlaskTest() }>
+            <Text> Start!! </Text>
+          </TouchableHighlight>
+
+        </View>
+
+      </View>
     </View>
   );
 };
@@ -17,3 +47,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const circleStyle = StyleSheet.create({
+  circle:{
+    borderRadius: Math.round(Dimensions.get('window').width + Dimensions.get('window').height) / 2,
+    width: Dimensions.get('window').width * 0.5,
+    height: Dimensions.get('window').width * 0.5,
+    backgroundColor:'#10ff10',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
